a55b0d934429801549480fc6cef7d80453d45827
B1lkao <hrabousolesia@gmail.com>
2023-05-01 11:47:07 +0300

    Доробила дз

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
 require("dotenv").config()
-
 const authRouter = require('./routes/api/user-routes')
 const contactsRouter = require('./routes/api/contacts-routes')
-
 const app = express()
-@@ -14,6 +15,7 @@ app.use(logger(formatsLogger))
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(express.static("public"));
 
 app.use('/api/users', authRouter);
 app.use('/api/contacts', contactsRouter)
-
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
